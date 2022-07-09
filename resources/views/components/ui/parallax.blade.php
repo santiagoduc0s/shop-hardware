f1adec47f2925be4c71c7711db44d31070079464
Santiago Ducos <santidu200@gmail.com>
2022-07-09 16:58:17 -0300

    parallax and sliders

--- resources/views/components/ui/parallax.blade.php
+++ resources/views/components/ui/parallax.blade.php
@@ -1,18 +1,20 @@
 <!-- Parallax banner -->
-<section class="p-0">
-    <div class="full-banner parallax text-center p-left">
-        <img src="{{ $background->image }}" alt="" class="bg-img blur-up lazyload">
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <div class="banner-contain">
-                        <h2>2021</h2>
-                        <h3>fashion trends</h3>
-                        <h4>special offer</h4>
+@if ($background)
+    <section class="p-0">
+        <div class="full-banner parallax text-center p-left">
+            <img src="{{ $background->image }}" alt="" class="bg-img blur-up lazyload">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <div class="banner-contain">
+                            <h2>2021</h2>
+                            <h3>fashion trends</h3>
+                            <h4>special offer</h4>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
+@endif
 <!-- Parallax banner end -->
